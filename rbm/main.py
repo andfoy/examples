8276c7e6486fb2c763c1b4e94f27a82cfb3e4a7a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-03-31 11:27:14 -0500

    RBM last revision

--- rbm/main.py
+++ rbm/main.py
@@ -11,12 +11,12 @@ from torch.nn.parameter import Parameter
 class RBM(nn.Module):
     def __init__(self, D_in, H, D_out, N):
         super(RBM, self).__init__()
-        self.drop = nn.Dropout(dropout)
-        self.visible_units = nn.Linear(D_in, H)
-        self.hidden_units = nn.Linear(H, d_out)
-        self.hidden_units.weight = self.visible_units.weight.transpose()
-        self.N = N
-        self.init_weights()
+        # self.drop = nn.Dropout(dropout)
+        # self.visible_units = nn.Linear(D_in, H)
+        # self.hidden_units = nn.Linear(H, d_out)
+        # self.hidden_units.weight = self.visible_units.weight.transpose()
+        # self.N = N
+        # self.init_weights()
 
     def init_weights(self):
         initrange = 0.1
@@ -37,13 +37,37 @@ class RBMLayer(autograd.Function):
         stdv = 1.0 / math.sqrt(self.weight.size(1))
         self.weight.data.uniform_(-stdv, stdv)
 
+    def configuration_grad(self, vis_state, hid_state):
+        grad = vis_state.mm(hid_state.t()) / vis_state.size(2)
+        return grad
+
     def binarize(self, x):
         rand_source = torch.Tensor(x.size()).uniform_(0, 1)
         return 0 + x > rand_source
 
     def vis_to_hid(self, x):
-        return x.mm(self.weight)
+        return nn.Sigmoid(x.mm(self.weight))
+
+    def hid_to_vis(self, x):
+        return nn.Sigmoid(self.weight.t().mm(x))
 
     def forward(self, input):
-        vis_probs = self.binarize(input)
-        return self.vis_to_hid(vis_probs)
+        vis_bin = self.binarize(input)
+        hid_probs = self.vis_to_hid(vis_bin)
+        self.save_for_backward(hid_probs)
+        return hid_probs
+
+    def backward(self, grad):
+        hid_probs = self.saved_tensors
+        hid_bin = self.binarize(hid_probs)
+        grad1 = self.configuration_grad(hid_bin)
+        for i in range(0, self.N):
+            vis_probs = self.hid_to_vis(hid_bin)
+            vis_bin = self.binarize(vis_probs)
+            hid_probs = self.vis_to_hid(vis_bin)
+            if i == self.N - 1:
+                hid_bin = hid_probs
+                break
+            hid_bin = self.binarize(hid_probs)
+        grad2 = self.configuration_grad(hid_bin)
+        return grad1 - grad2
